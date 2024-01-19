9c080d16ad4fed09098e368d0a1919df3ed171d7
sisqodataclub <103331558+sisqodataclub@users.noreply.github.com>
2024-01-19 04:51:30 +0000

    Update urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
 	path('add/', views.cart_add, name="cart_add"),
 	path('delete/', views.cart_delete, name="cart_delete"),
 	path('update/', views.cart_update, name="cart_update"),
-	path('create_payment_link/', views.create_payment_link, name='create_payment_link'),
 ]
